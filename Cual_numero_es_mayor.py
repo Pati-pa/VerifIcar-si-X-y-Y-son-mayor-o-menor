21b3087487d8874342f9b0866e9ff2ef627a6eba
Pati-pa <javier2234518@correo.uis.edu.co>
2023-03-03 15:48:17 -0500

    Se agrego diagrama de flujo y se realizo codigo

---
+++ Cual_numero_es_mayor.py
@@ -0,0 +1,21 @@
+# Verificar si un numero X es mayor que Y
+
+
+print("----------------------------------")
+print("-------------Mayor o Menor----------")
+print("----------------------------------")
+#input
+
+X= int(input("Ingrese X: "))
+Y = int(input("INGRESE Y: "))
+
+
+#Processing
+
+print("-------------------------------")
+print("------------RESULTADO----------")
+print("-------------------------------")
+if X>Y:
+    print("El numero " + str(X) + " es mayor que el numero " + str(Y))
+else:
+    print("El numero " + str(Y) + " es mayor que el numero " + str(X))
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # VerifIcar-si-X-y-Y-son-mayor-o-menor
 Hacer la verificación entre X y Y cual de los dos es mayor y cual menor
+
+# Diseño
+
+### Diagrama de flujo
+
+![Diagrama de fujo](diagrama.png "Diagrama de flujo")
